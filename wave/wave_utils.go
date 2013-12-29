21d4dd2ce5ac19bf50682c926fb038762832b3b3
hendrixjl <hendrix.john64@gmail.com>
2013-12-29 08:39:44 -0600

    add some comments

--- wave/wave_utils.go
+++ wave/wave_utils.go
@@ -8,6 +8,7 @@ import (
 	"encoding/binary"
 )
 
+// read into a preallocated buffer
 func Read_into_buffer(reader *bufio.Reader, size int, buf []byte) {
 	for totalRead := 0; totalRead < size; {
 		// read a chunk
@@ -17,6 +18,7 @@ func Read_into_buffer(reader *bufio.Reader, size int, buf []byte) {
 	}
 }
 
+// read a certain number of bytes and return in a slice
 func Read_bytes(reader *bufio.Reader, size int) []byte {
 	buf := make([]byte, size)
 	Read_into_buffer(reader, size, buf)
@@ -27,7 +29,8 @@ func Read_fixed_string(reader *bufio.Reader, size int) string {
 	return string(Read_bytes(reader, size))
 }
 
-
+// binary.Read only supports 64-bit integers
+// adapt to 32 bit integer
 func Read_uint32(reader *bufio.Reader) uint32 {
 	const (
 		UINT32_SIZE = 4
@@ -45,6 +48,7 @@ func Read_uint32(reader *bufio.Reader) uint32 {
 	return uint32(answer)
 }
 
+// Adapt binary.Read to a 16 bit integer
 func Read_uint16(reader *bufio.Reader) uint16 {
 	const (
 		UINT16_SIZE = 2
