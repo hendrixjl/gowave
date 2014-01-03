df24000c721a50235c6c0f83986ca20bdd62729b
hendrixjl <hendrix.john64@gmail.com>
2014-01-03 16:52:38 -0600

    Create utils_test.go

---
+++ wave/utils_test.go
@@ -0,0 +1,58 @@
+package wave
+
+import (
+	"testing" 
+	"bufio"
+	"fmt"
+)
+
+
+type fakeReader struct {
+	b bool
+}
+
+func (this *fakeReader) Read(p []byte) (n int, err error) {
+	p[0] = 1
+	p[1] = 2
+	p[2] = 3
+	p[3] = 4
+	p[4] = 5
+	p[5] = 6
+	p[6] = 7
+	p[7] = 8
+	n = 8
+	err = nil
+	return
+}
+
+func TestRead_into_buffer_happy(t *testing.T) {
+	var freader fakeReader
+	reader := bufio.NewReader(&freader)
+	buf := make([]byte, 5)
+	bytesRead, err := Read_into_buffer(reader, 4, buf)
+	if (err != nil) {
+		t.Error(fmt.Sprintf("Error returned! (%s)", err));
+	}
+	if (bytesRead != 4) {
+		t.Error(fmt.Sprintf("Wrong number of bytes Read! (%d)", bytesRead));
+	}
+	if buf[0] != 1 ||
+	   buf[1] != 2 ||
+	   buf[2] != 3 ||
+	   buf[3] != 4 {
+		t.Error("Values not correct!")
+	}
+}
+
+func TestRead_into_buffer_tooSmall(t *testing.T) {
+	var freader fakeReader
+	reader := bufio.NewReader(&freader)
+	buf := make([]byte, 2)
+	bytesRead, err := Read_into_buffer(reader, 4, buf)
+	if (err == nil) {
+		t.Error("No error returned!")
+	}
+	if (bytesRead != 0) {
+		t.Error("No bytes should be read!")
+	}
+}
